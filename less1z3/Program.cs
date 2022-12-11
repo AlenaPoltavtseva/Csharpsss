0bbbad8f6d72f88c37cf979737492ce20728c5a6
Alena Poltavtseva <alena.panch@yandex.ru>
2022-12-11 19:44:38 +0300

    add less1z3

---
+++ less1z3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+for(int i= -n; i <=n; i++)
+{ 
+    Console.Write(i);
+    Console.Write(", ");
+}
+// с введением -n не работает
